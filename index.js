48d85f4f93984c6fabda987ce892d73940c91eeb
similoluwa <simiprech@gmail.com>
2023-09-16 12:56:47 -0700

    difference between var,const and let

---
+++ index.js
@@ -0,0 +1,20 @@
+// alert("HELLO WORLD")
+//document.write("Hello Septa People")
+//console.log("Hi guys,its cool to learn js")
+
+const Name="Similoluwa"
+const Place="ikeja"
+const School="LASU"
+
+
+
+
+
+console.log("My name is"+" "+Name+" "+"I live at"+" "+Place+","+"I attend"+" "+School)
+
+//The var statement declares a variable.
+
+//let is used to declare a variable that is block scoped in JavaScript
+
+//const declaration maintains consnant value,they can only be declared within the blocks and can not be outdated or redeclared
+
