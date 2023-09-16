aeca08f36f28b356c50d0e52d19c982dc5330ef2
similoluwa <simiprech@gmail.com>
2023-09-16 13:39:02 -0700

    javascript functions

--- index.js
+++ index.js
@@ -18,3 +18,31 @@ console.log("My name is"+" "+Name+" "+"I live at"+" "+Place+","+"I attend"+" "+S
 
 //const declaration maintains consnant value,they can only be declared within the blocks and can not be outdated or redeclared
 
+function addition(a,b){
+    console.log(a + b)
+    console.log(a + "+" + b + "=", a + b)
+}
+
+addition(3,2)
+addition(2,4)
+addition(5,2)
+addition(8,2)
+
+function subtraction(a,b){
+    console.log(a - b)
+    console.log(a + "-" + b + "=", a - b)
+}
+subtraction(5,3)
+subtraction(4,2)
+subtraction(8,2)
+subtraction(9,5)
+
+function mutiply(a,b){
+    console.log(a * b)
+    console.log(a + "*" + b * "=", a * b)
+}
+mutiply(5,3)
+mutiply(4,2)
+mutiply(8,2)
+mutiply(9,5)
+
