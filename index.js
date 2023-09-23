30502e51c711c7c2222f82b21e9fbaf8026a5e98
similoluwa <simiprech@gmail.com>
2023-09-23 15:41:16 -0700

    voterscommit

--- index.js
+++ index.js
@@ -46,3 +46,112 @@ mutiply(4,2)
 mutiply(8,2)
 mutiply(9,5)
 
+
+let woman = {
+    skinColor:"dark skin",
+    age:"30",
+    Height:"7ft",
+};
+console.log(woman.age);
+woman.skinColor="dark"
+console.log(woman.skinColor"dark skin");
+
+
+//Array
+let animal = ["Dog", "Bat"]
+
+let cars = ["Lexus", "Venza", "Corolla", "Micra"];
+
+//access an array
+console.log(cars);
+console.log(cars[2]);
+
+cars[3] = "Toyota"
+console.log(cars);
+
+let person = [
+
+{
+
+  gender:'female',
+  ageBracket: '15-20',
+  School:"queens college",
+  adress: {
+    street : "adekunle street",
+    city: "lagos"
+  }
+}
+{
+   gender:'male',
+   ageBracket: '15-20',
+   School:"Kings college",
+   adress: {
+    street : "adekunle street",
+    city: "ogun"
+}
+}
+]
+console.log(person[0].gender);
+console.log(person[1].gender);
+
+person[0].address.city="ogun";
+person[1].adress.city="ogun";
+
+person[2]="Simloluwa"
+
+
+
+
+let usersage = prompt("please enter your age");
+console.log (usersage)
+let citizenship =prompt("Are you a citizen?");
+console.log (citizenship)
+  
+if (usersage =>18 && citizenship == "yes"){
+    console.log ("user is eligible to vote")
+}
+else {
+    console.log("user is not eligible to vote")}
+    
+
+
+
+    let usersage = prompt("please enter your age");
+    console.log (usersage);
+    let citizenship = prompt("Are you a citizen? (yes or no)");
+    console.log (citizenship);
+      
+    if (usersage >=18 && citizenship === "yes"){
+        console.log ("user is eligible to vote");
+    }
+    else {
+        console.log("user is not eligible to vote")}
+        
+    
+            
+
+         
+
+
+
+
+//conditional statements
+
+// if (condition){
+//    //code to be executed if the condition is true
+//}
+18 > 
+
+//!= ==
+//!== ===
+
+
+
+
+let age =18;
+
+
+
+if ( < 20) {
+
+}
